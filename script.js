0ff5394978ca910efab80d5ece5b25865cccb19f
Ashish Jaiswal <67005570+Ashishrxl@users.noreply.github.com>
2023-07-24 16:15:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ $(window).on('load', function(){
         counter++;
         if(counter == imageCount){
             $('#slider ul').clone().appendTo('#slider');
-            $('#slider ul').last().css('left', imageWidth + 'px');
+           // $('#slider ul').last().css('left', imageWidth + 'px');
             leftPosition = `-${totalWidth}`;
             $('#slider ul').last().animate({left:0}, 700, "easeInQuad");
             $('#slider ul').first().animate({left: leftPosition}, 700, "easeInQuad", function(){
