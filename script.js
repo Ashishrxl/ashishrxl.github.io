35d88ff6669a2dc662cef7aaffcee3caa3b1fa41
Ashish Jaiswal <67005570+Ashishrxl@users.noreply.github.com>
2023-07-24 11:02:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,25 @@ $(window).on('load', function(){
     const imageCount = $('#slider ul li').length;
     const imageWidth = $('#slider ul li img').first().width();
     const totalWidth = (imageCount * imageWidth)+ 'px';
-    alert(totalWidth);
+    let position = 0;
+    let counter = 0;
+    
+    $('#next').click(function(){
+        counter++;
+        if(counter == imageCount){
+            $('#slider ul').clone().appendTo('#slider');
+            $('#slider ul').last().css('left', imageWidth + 'px');
+            leftPosition = -totalWidth;
+            $('#slider ul').last().animate({left:0}, 700, 'easeInQuad');
+            $('#slider ul').first().animate({left: leftPosition}, 700, 'easeInQuad', function(){
+                $('#slider ul').first().remove();
+            });
+            counter = 0;
+
+        }else{
+
+        }
+
+    });
 
 });
