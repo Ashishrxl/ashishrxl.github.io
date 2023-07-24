908b831543efe2d99777e4782082c4d6a8e6a0f6
Ashish Jaiswal <67005570+Ashishrxl@users.noreply.github.com>
2023-07-24 11:43:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ $(window).on('load', function(){
 
         }
         else{
+            alert('hi');
             leftPosition = `-${counter * imageWidth}px`;
             alert(leftPosition);
             $('#slider ul').animate({left: leftPosition}, 700, 'easeInQuad');
