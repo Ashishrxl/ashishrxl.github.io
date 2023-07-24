dc663ead4d3ca32df3df136ba5d43420fdcea945
Ashish Jaiswal <67005570+Ashishrxl@users.noreply.github.com>
2023-07-24 10:43:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ $(window).on('load', function(){
     'use strict';
     const imageCount = $('#slider ul li').length;
     const imageWidth = $('#slider ul li img').first().width();
-    alert(imageWidth);
+    const totalWidth = (imageCount * imageWidth)+ 'px';
+    alert(totalWidth);
 
 });
