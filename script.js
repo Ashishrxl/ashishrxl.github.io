7ed8583667ff858c4feb7d46c4e5f0f94d2ba313
Ashish Jaiswal <67005570+Ashishrxl@users.noreply.github.com>
2023-07-27 10:38:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
     "use strict";
     let counter = 1;
     function contentRotator(){
-        $('#container p:nth-child(${counter}').fadeIn(2000, function(){
+        $('#container p:nth-child(${counter})').fadeIn(2000, function(){
             if( $(this).is('#container p:last-child')){
                 setTimeout (function(){
                     $('#container p:nth-child(${counter})').fadeOut(2000, function(){
