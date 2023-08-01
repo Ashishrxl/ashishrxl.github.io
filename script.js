e2ddcad093c0d40dd544b985b7ed1a66142763e9
Ashish Jaiswal <67005570+Ashishrxl@users.noreply.github.com>
2023-08-01 10:22:29 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+$('nav ul li a').click(function(){
+    var thisSection = $(this).attr('href');
+    var thisLink = $(this);
+    $('html, body').stop().animate({
+        scrollTop: $(thisSection).offset.top -200
+    }, 800, 'easeOutCirc', function(){
+        $('nav ul li a').removeAttr('class');
+        $(thisLink).addClass('selected');
+
+    });
+
+});
+
+$(window).on('load', function(){
+    var posts = $('section');
+    var pageTop;
+    var postPos;
+    $(window).scroll(function(){
+        postPos = $(posts[0]).offset().top;
+        pageTop = $(window).scrollTop();
+        alert(`${pageTop} and ${postPos}`);
+
+
+    });
+
+});
+
