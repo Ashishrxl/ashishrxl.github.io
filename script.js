3350d762c819f794f43ecda8f0c34a22c6e41dc9
Ashish Jaiswal <67005570+Ashishrxl@users.noreply.github.com>
2023-08-01 21:29:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -53,3 +53,19 @@ $(window).on('load', function(){
 
 });
 
+/*
+$(window).on('load', function(){
+  $('.flexslider').flexslider(
+    {
+      animation: 'slide',
+      slideshowSpeed: 2000,
+      direction: 'vertical',
+      reverse: true,
+      pauseOnHover: true
+    }
+  );
+});
+
+
+
+*/
