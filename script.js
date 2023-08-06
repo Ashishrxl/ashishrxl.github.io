8b84e5b11ef6b62ac47cf3931a34e8055d0c85b3
Ashish Jaiswal <67005570+Ashishrxl@users.noreply.github.com>
2023-08-06 21:35:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,8 @@ $('nav ul li a').click(function(){
     var thisLink = $(this);
     $('html, body').stop().animate({
         scrollTop: $(thisSection).offset.top -200
-    }, 800, 'easeOutCirc', function(){
-        $('nav ul li a').removeAttr('class');
-        $(thisLink).addClass('selected');
-
+    }, 800, 'easeOutCirc');
+    return false;
     });
 
 });
