7578b7ad67faeb0c4b5e6e7bdbcd4417cd4e0f2b
Ashish Jaiswal <67005570+Ashishrxl@users.noreply.github.com>
2023-08-06 21:32:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/*
+
 $('nav ul li a').click(function(){
     var thisSection = $(this).attr('href');
     var thisLink = $(this);
@@ -11,8 +11,6 @@ $('nav ul li a').click(function(){
     });
 
 });
-*/
-
 $(window).on('load', function(){
     var allLinks = $('nav ul li a');
     var posts = $('section');
@@ -20,6 +18,7 @@ $(window).on('load', function(){
     var postPos;
     var counter = 0;
     var prevCounter = 0;
+    var doneResizing;
     var postTops =[];
     posts.each(function(){
         postTops.push(Math.floor($(this).offset().top));
@@ -30,25 +29,41 @@ $(window).on('load', function(){
         pageTop = $(window).scrollTop()+210;
         if(pageTop > postTops[counter+1]){
             counter++;
-            alert(`Scrolling down! ${counter+1}`);
+            console.log(`Scrolling down! ${counter}`);
         }
         else if(counter>0 && pageTop<postTops[counter]){
             counter--;
-            alert(`Scrolling up! ${counter+1}`);
+            console.log(`Scrolling up! ${counter}`);
         }
         if(counter != prevCounter){
             $(allLinks).removeAttr('class');
             $('nav ul li a').eq(counter).addClass('selected');
             prevCounter = counter;
         }
-        
+
 
     });
+    $(window).on('resize', function(){
+      clearTimeout(doneResizing);
+      doneResizing = setTimeout(function(){
+        resetPagePosition();
+      }, 500);
+    });
+
+    function resetPagePosition(){
+      postTops =[];
+      posts.each(function(){
+        postTops.push(Math.floor($(this).offset().top));
+      });
+      var pagePosition = $(window).scrollTop()+210;
+      counter=0;
+      for (var i=0; i<postTops.length; i++){
+        if(pagePosition > postTops[i]){ counter++;}
+      }
+      counter--;
+      $(allLinks).removeAttr('class');
+      $('nav ul li a').eq(counter).addClass('selected');
+
+    }
 
 });
-
-
-
-
-
-
