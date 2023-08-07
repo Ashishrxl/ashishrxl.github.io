dd2c3708aa6ff7462b08ae4abfbc46a3867dbdd3
Ashish Jaiswal <67005570+Ashishrxl@users.noreply.github.com>
2023-08-07 10:09:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const navLinks = document.querySelectorAll('nav ul li a');
 navLinks.forEach(function(eachLink){
-    eacLink.addEventListener('click', smoothScroll);
+    eacLink.addEventListener('click', smoothScroll());
 });
 function smoothScroll(event){
     event.preventDefault();
