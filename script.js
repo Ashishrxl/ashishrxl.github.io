3d9a46b75ea5dd94d8b62fcae986eddadee9eaa4
Ashish Jaiswal <67005570+Ashishrxl@users.noreply.github.com>
2023-08-07 09:55:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,67 +1,10 @@
-
-$('nav ul li a').click(function(){
-    var thisSection = $(this).attr('href');
-    var thisLink = $(this);
-    $('html, body').stop().animate({
-        scrollTop: $(thisSection).offset().top -200
-    }, 800, 'easeOutCirc');
-    return false;
-
-
+const navLinks = document.querySelectorAll('nav ul li a');
+navLinks.forEach(function(eachLink){
+    eacLink.addEventListener('click', smoothScroll);
 });
-$(window).on('load', function(){
-    var allLinks = $('nav ul li a');
-    var posts = $('section');
-    var pageTop;
-    var postPos;
-    var counter = 0;
-    var prevCounter = 0;
-    var doneResizing;
-    var postTops =[];
-    posts.each(function(){
-        postTops.push(Math.floor($(this).offset().top));
-
-    });
-    //alert(postTops);
-    $(window).scroll(function(){
-        pageTop = $(window).scrollTop()+210;
-        if(pageTop > postTops[counter+1]){
-            counter++;
-            console.log(`Scrolling down! ${counter}`);
-        }
-        else if(counter>0 && pageTop<postTops[counter]){
-            counter--;
-            console.log(`Scrolling up! ${counter}`);
-        }
-        if(counter != prevCounter){
-            $(allLinks).removeAttr('class');
-            $('nav ul li a').eq(counter).addClass('selected');
-            prevCounter = counter;
-        }
-
-
-    });
-    $(window).on('resize', function(){
-      clearTimeout(doneResizing);
-      doneResizing = setTimeout(function(){
-        resetPagePosition();
-      }, 500);
-    });
-
-    function resetPagePosition(){
-      postTops =[];
-      posts.each(function(){
-        postTops.push(Math.floor($(this).offset().top));
-      });
-      var pagePosition = $(window).scrollTop()+210;
-      counter=0;
-      for (var i=0; i<postTops.length; i++){
-        if(pagePosition > postTops[i]){ counter++;}
-      }
-      counter--;
-      $(allLinks).removeAttr('class');
-      $('nav ul li a').eq(counter).addClass('selected');
-
-    }
-
-});
+function smoothScroll(event){
+    event.preventDefault();
+    const targetID = event.target.getAttribute('href');
+    const targetAnchor = document.querySelector(targetID);
+    alert(targetAnchor.getBoundingClientRect().top);
+}
